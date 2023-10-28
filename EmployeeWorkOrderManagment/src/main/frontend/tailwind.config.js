0bf354d7005412d514d8448ed05d1f17314d682c
Luke <nycthbre@gmail.com>
2023-10-28 02:15:48 +0200

    feaet(view): Basic site layout
    Adds homepage for application

--- EmployeeWorkOrderManagment/src/main/frontend/tailwind.config.js
+++ EmployeeWorkOrderManagment/src/main/frontend/tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["../resources/templates/**/*.{html,js}"],
+    content: ["../resources/templates/**/*.html", "../resources/js/*.js"],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                'windscreen': {'raw': '(min-aspect-ratio: 3/2'},
+                'tailscreen': {'raw': '(min-aspect-ratio: 13/20'},
+            },
+            keyframes: {
+                'open-menu': {
+                    '0%': { transform: 'scaleY(0)'},
+                    '80%': { transform: 'scaleY(1.2)'},
+                    '100%': { transform: 'scaleY(1)'}
+                },
+            },
+            animation: {
+                'open-menu': 'open-menu 0.5 ease-in-out forwards',
+            }
+        },
     },
     plugins: [],
 }
