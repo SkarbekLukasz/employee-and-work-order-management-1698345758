99665228bd324717d30ec345ac01aa0099dedb8e
Luke <nycthbre@gmail.com>
2023-11-02 18:02:33 +0100

    feat(login): Custom login page
    Adds custom login page with corresponding controller method and changes to footer layout.

--- EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/controller/HomepageController.java
+++ EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/controller/HomepageController.java
@@ -2,11 +2,17 @@ package ls.EmployeeWorkOrderManagment.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomepageController {
     @GetMapping("")
     public String getHomepage() {
         return "index";
+    }
+
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
     }
 }
