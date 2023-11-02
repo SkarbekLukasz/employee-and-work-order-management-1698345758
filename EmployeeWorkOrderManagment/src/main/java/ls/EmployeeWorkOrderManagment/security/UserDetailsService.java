af871d9b91f48b8bb38594f16001b8918bd2cfbd
Luke <nycthbre@gmail.com>
2023-11-02 18:01:28 +0100

    config(security): Custom authentication
    Adds custom authentication process for managing login page credentials.

--- EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/security/UserDetailsService.java
+++ EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/security/UserDetailsService.java
@@ -5,7 +5,9 @@ import ls.EmployeeWorkOrderManagment.service.UserService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsService implements org.springframework.security.core.userdetails.UserDetailsService {
     private final UserService userService;
 
