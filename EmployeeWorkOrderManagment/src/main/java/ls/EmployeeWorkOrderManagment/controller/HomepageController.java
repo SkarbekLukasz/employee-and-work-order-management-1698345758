0e8f86ef1089cfe6ae39352df62730ab3d308adb
Luke <nycthbre@gmail.com>
2023-11-03 00:01:18 +0100

    refactor(): Mapping for homepage

--- EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/controller/HomepageController.java
+++ EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/controller/HomepageController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomepageController {
-    @GetMapping("")
+    @GetMapping("/")
     public String getHomepage() {
         return "index";
     }
