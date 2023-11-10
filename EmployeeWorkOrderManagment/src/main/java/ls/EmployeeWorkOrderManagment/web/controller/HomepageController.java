793ac5c9359dd458a08fe56d50b09c5d8e1ed6c4
Luke <nycthbre@gmail.com>
2023-11-10 18:11:41 +0100

    feat(): Mapping for error HTML page

--- EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/web/controller/HomepageController.java
+++ EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/web/controller/HomepageController.java
@@ -1,6 +1,7 @@
 package ls.EmployeeWorkOrderManagment.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,5 +9,10 @@ public class HomepageController {
     @GetMapping("/")
     public String getHomepage() {
         return "index";
+    }
+
+    @GetMapping("/error")
+    public String getErrorPage(Model model) {
+        return "message";
     }
 }
