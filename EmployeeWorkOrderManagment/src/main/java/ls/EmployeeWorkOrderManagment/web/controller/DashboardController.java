a2d8dec462c8b08186e40621520c797bf96d11c0
Luke <nycthbre@gmail.com>
2023-11-28 20:56:46 +0100

    refactor(): Imports optimize

--- EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/web/controller/DashboardController.java
+++ EmployeeWorkOrderManagment/src/main/java/ls/EmployeeWorkOrderManagment/web/controller/DashboardController.java
@@ -1,19 +1,9 @@
 package ls.EmployeeWorkOrderManagment.web.controller;
 
-import ls.EmployeeWorkOrderManagment.service.RoleService;
-import ls.EmployeeWorkOrderManagment.service.UserService;
-import ls.EmployeeWorkOrderManagment.web.dto.role.RoleDto;
-import ls.EmployeeWorkOrderManagment.web.dto.user.UserSiteRenderDto;
-import ls.EmployeeWorkOrderManagment.web.error.UserNotFoundException;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/dashboard")
